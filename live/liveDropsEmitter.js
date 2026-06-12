d411a41f8fdf9ecbcd3375c23ac4a23254195944
delihop <syn82829@gmail.com>
2026-06-12 23:21:13 +0300

    Update liveDropsEmitter.js

--- live/liveDropsEmitter.js
+++ live/liveDropsEmitter.js
@@ -1,1 +1,22 @@
+const { addLiveDrop, getLiveDrops } = require("./liveDropsStore")
 
+let ioInstance = null
+
+function initLiveDrops(io) {
+  ioInstance = io
+}
+
+function emitLiveDrop(drop) {
+  addLiveDrop(drop)
+
+  if (!ioInstance) {
+    return
+  }
+
+  ioInstance.emit("live:drops", getLiveDrops())
+}
+
+module.exports = {
+  initLiveDrops,
+  emitLiveDrop,
+}
