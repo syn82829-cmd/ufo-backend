eba018a2911865ed07fa4a3119c8312562777de9
delihop <syn82829@gmail.com>
2026-06-12 23:19:27 +0300

    Update liveDropsStore.js

--- live/liveDropsStore.js
+++ live/liveDropsStore.js
@@ -1,1 +1,18 @@
+const liveDrops = []
 
+function addLiveDrop(drop) {
+  liveDrops.unshift(drop)
+
+  if (liveDrops.length > 20) {
+    liveDrops.length = 20
+  }
+}
+
+function getLiveDrops() {
+  return liveDrops
+}
+
+module.exports = {
+  addLiveDrop,
+  getLiveDrops,
+}
