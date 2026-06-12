234786e46ac5015b578a06416e9e517b97a66c7c
delihop <syn82829@gmail.com>
2026-06-12 23:08:06 +0300

    Update fakeDrops.js

--- live/fakeDrops.js
+++ live/fakeDrops.js
@@ -1,1 +1,33 @@
+const { emitLiveDrop } = require("./liveDropsEmitter")
 
+const fakeItems = [
+  "/drops/Baklajan.png",
+  "/drops/Dog.png",
+  "/drops/Fen.png",
+  "/drops/HeroicHelmet.png",
+  "/drops/IonicDryer.png",
+]
+
+function scheduleFakeDrop() {
+  const delay =
+    120000 +
+    Math.random() * 180000
+
+  setTimeout(() => {
+    const image =
+      fakeItems[
+        Math.floor(Math.random() * fakeItems.length)
+      ]
+
+    emitLiveDrop({
+      image,
+      fake: true,
+    })
+
+    scheduleFakeDrop()
+  }, delay)
+}
+
+module.exports = {
+  scheduleFakeDrop,
+}
