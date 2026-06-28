8b1ada256ec9140f53076ebeff784ef51e080a49
delihop <syn82829@gmail.com>
2026-06-28 16:48:15 +0300

    Use webp images for real live drops

--- routes/caseRoutes.js
+++ routes/caseRoutes.js
@@ -70,13 +70,11 @@ router.post("/case/open", async (req, res) => {
       }
     })
 
-    console.log("LIVE PNG:", result.inventoryItem.png)
-
     emitLiveDrop({
-  image: `/drops/${result.inventoryItem.png}.png`,
-  name: result.inventoryItem.dropName,
-  price: result.inventoryItem.priceStars || 0,
-})
+      image: `/drops/${result.inventoryItem.png}.webp`,
+      name: result.inventoryItem.dropName,
+      price: result.inventoryItem.priceStars || 0,
+    })
 
     res.json({
       balance: result.balance,
